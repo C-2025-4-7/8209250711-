2ba729150fc766de8218e4321b4cb915b65d6e82
X-commrad <3134488983@qq.com>
2025-11-17 20:25:45 +0800

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float x;
+    cin >> x;
+    if (x > 0 && x < 1)
+    {
+        cout << 3 - 2 * x;
+    }
+    else
+    {
+        if (x >= 1 && x < 5)
+        {
+            cout << float(1 / (2 * x)) + 1;
+        }
+        if (x >= 5 && x < 10)
+        {
+            cout << x * x;
+        }
+
+    }
+    return 0;
+}
