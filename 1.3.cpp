0d5f2fb87addc543bf9fdc71f879f1f922823e46
X-commrad <3134488983@qq.com>
2025-11-17 20:21:22 +0800

    Add files via upload

---
+++ 1.3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "short length:" << sizeof(short) << endl;
+    cout << "int length:" << sizeof(int) << endl;
+    cout << "long length:" << sizeof(long) << endl;
+    cout << "long long length:" << sizeof(long long) << endl;
+    cout << "float length:" << sizeof(float) << endl;
+    cout << "double length:" << sizeof(double) << endl;
+    cout << "char length:" << sizeof(char) << endl;
+    cout << "bool length:" << sizeof(bool) << endl;
+    cout << "string length:" << sizeof(string) << endl;
+}
