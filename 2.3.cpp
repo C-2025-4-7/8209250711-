fcf6d6e6bec8d683fad39060d7f21ecedbb334a0
X-commrad <3134488983@qq.com>
2025-11-17 20:27:21 +0800

    Add files via upload

---
+++ 2.3.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+    if (a < b + c && b < a + c && c < a + b)
+    {
+        cout << a + b + c << endl;
+        if (a == b || b == c || a == c)
+            cout << "Yes";
+        else
+            cout << "No";
+    }
+    else
+    {
+        cout << "No triangle";
+    }
+    return 0;
+}
