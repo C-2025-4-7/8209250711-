6a7c53e8a8051ce2c7564fd069dce20f034e0ed9
X-commrad <3134488983@qq.com>
2025-11-17 20:24:14 +0800

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    if (a >= 'a' && a <= 'z')
+    {
+        cout << (char)(a - 32);
+    }
+    else
+        cout << a + 1;
+    return 0;
+}
