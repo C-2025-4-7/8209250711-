4d427c8f9f1d0a2b69edc18abb31f30a410d4358
X-commrad <3134488983@qq.com>
2025-11-17 20:01:44 +0800

    Add files via upload

---
+++ gaizheng.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+    int k = 0;          // 先定义k并赋值
+    int i = k + 1;
+    cout << i++ << endl;
+
+    int j = 1;          // 改用不同的变量名
+    cout << j++ << endl;
+    cout << "Welcome to C++!" << endl;
+    return 0;
+}
